fb32228a48bf7bc300629f9e54648cc96cd18a60
John Purcell <locke.renard@gmail.com>
2014-06-29 15:49:07 -0600

    more framework for blueprint/activity classes

--- eve_blueprint.py
+++ eve_blueprint.py
@@ -2,9 +2,24 @@
 
 import json
 
+activity_conversion = {
+	#activityID,activitySTR
+	"0":"None",
+	"1":"Manufacturing",
+	"2":"DEV--Researching Technology",
+	"3":"Researching Time Productivity",
+	"4":"Researching Material Productivity",
+	"5":"Copying",
+	"6":"DEV--Duplicating",
+	"7":"Reverse Engineering",
+	"8":"Invention"
+	}
 class Blueprint:
 	def __init__ (self):
 		None
+		activities = {}
+		blueprintTypeID = 0
+		maxProductionLimit = 1
 		
 	def json_parse(self,JSON_str):
 		None
@@ -16,3 +31,20 @@ class Blueprint:
 	def bill_of_materials (self, activity_index):
 		bill_obj = None
 		return bill_obj
+		
+class Blueprint_activity:	##Make child of Blueprint?
+	def __init__ (self):
+		None
+		activityID = 0
+		activityName = ""
+		skills = {}
+		materials = {}	## materialID : quantity
+		products = {} ##make pretty return function?
+		time = 0
+		
+	def json_load(self,JSON_str):
+		None
+	
+	def obj_load(self,obj):
+		None
+	
