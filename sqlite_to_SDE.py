11ddafcf7629c33ea37da3200bd078b2e9c837b4
John Purcell <locke.renard@gmail.com>
2014-06-29 20:03:50 -0600

    sqlite_to_SDE works as advertised
    Slow as balls.  Need to figure out multiple inserts to make it run faster

--- sqlite_to_SDE.py
+++ sqlite_to_SDE.py
@@ -3,6 +3,7 @@
 
 import sqlite3 as lite
 import MySQLdb
+import json
 import getopt
 import time
 import sys
@@ -29,6 +30,7 @@ sqlite_defaultPath = "C:\\Program Files (x86)\\CCP\\Singularity\\bin\\staticdata
 material_table = "crius_BPOmaterials"
 skill_table = "crius_BPOskills"
 time_table = "crius_BPOtimes"
+prods_table = "crius_BPOproducts"
 
 def SQLite_fetch():
 	global sqlite_con, sqlite_cur
@@ -110,6 +112,19 @@ def configMySQL():
 	mysql_con.commit()
 	print "created %s table" % time_table 
 	
+	mysql_cur.execute('''DROP TABLE IF EXISTS `%s`''' % prods_table)
+	mysql_con.commit()
+	mysql_cur.execute('''CREATE TABLE `%s`(
+			`blueprintID` int(32) NOT NULL,
+			`activityID`  int(32) NOT NULL,
+			`proudctID`   int(32) NOT NULL,
+			`quantity`    int(32) NOT NULL DEFAULT 0,
+			PRIMARY KEY (blueprintID,activityID,proudctID))
+		ENGINE=InnoDB DEFAULT CHARSET=latin1''' % prods_table)
+	mysql_con.commit()
+	
+	print "created %s table" % prods_table 
+	
 	
 def main():
 	parseargs()
@@ -126,5 +141,56 @@ def main():
 		global mysql_con, mysql_cur
 		configMySQL()
 		
+		print "--Writing to SQL--"
+		for row in data:
+			blueprintID = row[0]
+			json_str = row[1]
+			print "\t%s" % blueprintID
+			BPO_obj = json.loads(json_str)
+			
+			SQL_str = ""
+						
+			#TODO: base BPO info
+			maxProductionLimit = BPO_obj["maxProductionLimit"]
+			
+			##This sucks, and is hard to read.  Fix it##
+			##TODO: batch SQL writes for speed##
+			for activityID,payload in BPO_obj["activities"].iteritems():
+				for data_type,data_payload in payload.iteritems():
+					if data_type == "materials":
+						for itemID,qty_obj in data_payload.iteritems():
+							mat_qty = qty_obj["quantity"]
+							#SQL_str = "%sINSERT INTO `%s` (blueprintID,activityID,materialID,quantity) VALUES (%s,%s,%s,%s);\n"\
+							#	% (SQL_str,material_table,blueprintID,activityID,itemID,mat_qty)
+							mysql_cur.execute('''INSERT INTO %s (blueprintID,activityID,materialID,quantity) 
+								VALUES (%s,%s,%s,%s)''' % (material_table,blueprintID,activityID,itemID,mat_qty))
+							mysql_con.commit()
+					elif data_type == "skills":
+						for skillID,level_obj in data_payload.iteritems():
+							skillLevel = level_obj["level"]
+							#SQL_str = "%sINSERT INTO `%s` (blueprintID,activityID,skillID,skillLevel) VALUES (%s,%s,%s,%s);\n"\
+							#	% (SQL_str,skill_table,blueprintID,activityID,skillID,skillLevel)
+							mysql_cur.execute('''INSERT INTO %s (blueprintID,activityID,skillID,skillLevel) 
+								VALUES (%s,%s,%s,%s)''' % (skill_table,blueprintID,activityID,skillID,skillLevel))
+							mysql_con.commit()
+					elif data_type == "time":
+						BPtime = data_payload
+						#SQL_str = "%sINSERT INTO `%s` (blueprintID,activityID,time) VALUES (%s,%s,%s);\n"\
+						#	% (SQL_str,time_table,blueprintID,activityID,BPtime)
+						mysql_cur.execute('''INSERT INTO %s (blueprintID,activityID,time) 
+							VALUES (%s,%s,%s)''' % (time_table,blueprintID,activityID,BPtime))
+						mysql_con.commit()
+					elif data_type == "products":
+						for itemID,result_obj in data_payload.iteritems():
+							prod_yield = result_obj["quantity"]
+							#SQL_str = "%sINSERT INTO %s (blueprintID,activityID,proudctID,quantity) VALUES (%s,%s,%s,%s);\n"\
+							#	% (SQL_str,prods_table,blueprintID,activityID,itemID,prod_yield)
+							mysql_cur.execute('''INSERT INTO %s (blueprintID,activityID,proudctID,quantity) 
+								VALUES (%s,%s,%s,%s)''' % (prods_table,blueprintID,activityID,itemID,prod_yield))
+							mysql_con.commit()
+			#TODO: write to SQL per-BPO	
+			#print SQL_str
+			#mysql_cur.executemany(SQL_str)
+			#mysql_con.commit()
 if __name__ == "__main__":
 	main()
