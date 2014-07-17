29ff6af4ffaeeb9b99d467d519f21e65c22af2f5
John Purcell <locke.renard@gmail.com>
2014-07-16 21:54:03 -0600

    added job parser to gdoc script

--- gdoc_scripts.js
+++ gdoc_scripts.js
@@ -1,7 +1,13 @@
 var base_CREST_URL = "http://public-crest.eveonline.com/";
 var test_CREST_URL = "http://public-crest-sisi.testeveonline.com/";
 var base_API_URL = "https://api.eveonline.com/char/AssetList.xml.aspx?";
-
+var jobType ={};
+JobType = {"1":"Manufacturing",
+		"3":"Researching Time Efficiency",
+		"4":"Researching Material Efficiency",
+		"5":"Copying",
+		"7":"Reverse Engineering",
+		"8":"Invention"}
 ////	UTILITIES	////
 function date_array(days)
 {
@@ -92,7 +98,153 @@ function __fetchMarketPrices(test_server)
   return json_obj;
 }
 
+function API_validateKey(keyID, vCode, expected_type, CAK_mask){
+   //Takes API info and throws errors if bad
+  
+  var parameters = {
+      method : "post",
+      payload :
+      "keyID=" + encodeURIComponent(keyID) +
+      "&vCode=" + encodeURIComponent(vCode)
+   };
+  try{//generic query error
+      Utilities.sleep(1000);
+    var info = UrlFetchApp.fetch("https://api.eveonline.com/account/APIKeyInfo.xml.aspx",parameters).getContentText();
+  }catch (e){
+    throw e;
+  }
+  var infoXML = Xml.parse(info,true);
+  var accessMask = infoXML.eveapi.result.key.getAttribute("accessmask").getValue();
+  var keytype = infoXML.eveapi.result.key.getAttribute("type").getValue();
+
+  
+  if (keytype != expected_type){
+    throw "Invalid key type:" +  keytype + "Expected: " + expected_type;
+  }
+  if ((Number(accessMask) & CAK_mask) == 0){
+    throw "Invalid access mask: " + accessMask + "Expected: " + CAK_mask; 
+  }
+  //make "EXPIRED" case
+}
 ////	GDOC FUNCS		////
+function getIndustryJobs(keyID, vCode, header_bool, verbose_bool, test_server)
+{
+	var personal_or_corp = 0;	//0=personal, 1=corp
+	try{
+		API_validateKey(keyID, vCode, "Account", 128);
+	}catch(err){
+		personal_or_corp = 1;
+		try{
+		API_validateKey(keyID, vCode, "Corporation", 128);
+		}catch(badkey){
+			return badkey;
+		}
+	}
+////SET UP ADDRESS CALL////	
+	parameters = {
+    method : "post",
+    payload :
+    "keyID=" + encodeURIComponent(keyID) +
+    "&vCode=" + encodeURIComponent(vCode)
+	};
+	Utilities.sleep(1000);
+	var API_addr = "";
+	var call_mod = "";
+	
+	//switch call for personal/corp
+	if (personal_or_corp == 0) call_mod = "char";
+	else call_mod = "corp";
+	
+	//switch call for test/live server
+	if(test_server) API_addr = "https://api.testeveonline.com/";
+	else API_addr = "https://api.eveonline.com/";
+	
+	var job_api = UrlFetchApp.fetch(API_addr+call_mod+"/IndustryJobs.xml.aspx", parameters);
+	var jobXML = Xml.parse(job_api,true);
+	var jobList = jobXML.eveapi.result.rowset.getElements("row");
+	
+	var return_array = [];
+	if(header_bool)
+	{
+		var header = []
+		if (verbose_bool)	header.push("jobID");
+		if (verbose_bool)	header.push("installerID");
+							header.push("installerName");
+							header.push("facilityID");
+		if (verbose_bool)	header.push("solarSystemID");
+							header.push("solarSystemName");
+							header.push("stationID");
+		if (verbose_bool)	header.push("activityID");
+							header.push("activity");
+		if (verbose_bool)	header.push("blueprintID");	
+							header.push("blueprintTypeID");
+							header.push("blueprintTypeName");
+		if (verbose_bool)	header.push("blueprintLocationID");						
+		if (verbose_bool)	header.push("outputLocationID");
+							header.push("runs");
+							header.push("cost");
+							header.push("teamID");
+		if (verbose_bool)	header.push("licensedRuns");	
+		if (verbose_bool)	header.push("probability");	
+		if (verbose_bool)	header.push("productTypeName");
+							header.push("status");
+							header.push("timeInSeconds");
+							header.push("startDate");
+							header.push("endDate");
+		if (verbose_bool)	header.push("pauseDate");
+		if (verbose_bool)	header.push("completedDate");
+		if (verbose_bool)	header.push("completedCharacterID");	
+
+		return_array.push(header);
+	}
+	for (var jobNum = 0; jobNum < jobList.length; jobNum++)
+	{
+		job_line = [];
+		
+		var activityID   = jobList[jobNum].getAttribute("activityID").getValue();	
+		var activityName = "";
+		try{
+			activityName = jobType[activityID];
+		}catch (err){
+			activityName = "UKN ACTIVITYID";
+		}
+		
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("jobID").getValue()));
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("installerID").getValue()));
+							job_line.push(		 jobList[jobNum].getAttribute("installerName").getValue());
+							job_line.push(Number(jobList[jobNum].getAttribute("facilityID").getValue()));
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("solarSystemID").getValue()));
+							job_line.push(		 jobList[jobNum].getAttribute("solarSystemName").getValue());
+							job_line.push(Number(jobList[jobNum].getAttribute("stationID").getValue()));
+		/*------------*/
+		if (verbose_bool)	job_line.push(Number(activityID));				
+							job_line.push(activityName);
+		/*------------*/					
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("blueprintID").getValue()));
+							job_line.push(Number(jobList[jobNum].getAttribute("blueprintTypeID").getValue()));
+							job_line.push(		 jobList[jobNum].getAttribute("blueprintTypeName").getValue());
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("blueprintLocationID").getValue()));						
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("outputLocationID").getValue()));
+							job_line.push(Number(jobList[jobNum].getAttribute("runs").getValue()));
+							job_line.push(Number(jobList[jobNum].getAttribute("cost").getValue()));
+							job_line.push(Number(jobList[jobNum].getAttribute("teamID").getValue()));
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("licensedRuns").getValue()));	
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("probability").getValue()));	
+		if (verbose_bool)	job_line.push(		 jobList[jobNum].getAttribute("productTypeName").getValue());
+		/*TODO ID->STR*/	job_line.push(Number(jobList[jobNum].getAttribute("status").getValue()));
+							job_line.push(Number(jobList[jobNum].getAttribute("timeInSeconds").getValue()));
+							job_line.push(		 jobList[jobNum].getAttribute("startDate").getValue());
+							job_line.push(		 jobList[jobNum].getAttribute("endDate").getValue());
+		if (verbose_bool)	job_line.push(		 jobList[jobNum].getAttribute("pauseDate").getValue());
+		if (verbose_bool)	job_line.push(		 jobList[jobNum].getAttribute("completedDate").getValue());
+		if (verbose_bool)	job_line.push(Number(jobList[jobNum].getAttribute("completedCharacterID").getValue()));	
+		
+		return_array.push(job_line);
+	}
+	
+	return return_array;
+}
+
 function getAvgVolume(days,item_id,region_id)
 {
   var market_obj = {};
