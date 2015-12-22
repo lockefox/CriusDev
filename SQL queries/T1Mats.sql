f0102f7cd8846455bf6d1af27914e6687df941ef
John Purcell <locke.renard@gmail.com>
2015-12-21 21:33:21 -0700

    updating SQL for better accuracy/utility

--- SQL queries/T1Mats.sql
+++ SQL queries/T1Mats.sql
@@ -1,4 +1,12 @@
-SELECT conv1.typeName, times.time, mats.materialtypeID, conv2.groupID, conv2.typeName as `matsTypeName`,mats.quantity
+SELECT	conv1.typeName as `Product`, 
+		conv1.typeid as `ProdID`, 
+		conv1.groupid as `ProdGrp`, 
+		times.time, 
+		conv2.typeName as `Material`,
+		mats.materialtypeID as `MatID`, 
+		conv2.groupID as `MatGrp`, 
+		mats.quantity,
+		prods.quantity as `yield`
 FROM industryactivitymaterials mats
 JOIN industryactivityproducts prods ON (mats.typeID=prods.typeID)
 JOIN industryactivity times ON (times.typeID = mats.typeID AND times.activityID=1) -- only get mfg times
@@ -10,3 +18,4 @@ WHERE conv1.marketGroupID IS NOT null
 AND (meta.metaGroupID = 2 OR conv1.groupID in (332,873))
 AND mats.activityID = 1
 AND conv1.groupID NOT IN (773, 774, 775, 776, 777, 778, 779, 781, 782, 786, 787,1233, 1223, 1232, 1234) -- filter out rigs
+GROUP BY conv1.typeid,mats.materialtypeID
